dc452a8dc7a6308d7df86e3332dd569d0146662b
ayushjoshi45 <ayushjoshi3725@gmail.com>
2026-02-03 22:32:37 +0530

    Fixed tailwind syntax

---
+++ frontend/src/components/AppliedJOBSCareer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AppliedJOBSCareer() {
+  return (
+    <div>
+        <h1>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore ipsa facere incidunt quaerat, quae neque voluptates obcaecati. Eos, totam! Deleniti commodi mollitia deserunt ipsa est!</h1>
+    </div>
+  )
+}
+
+export default AppliedJOBSCareer
